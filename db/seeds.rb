3b0e339f136c9d2c86f445e9e9a0ab16428f1bfd
alexlemia13 <alexlemia13@outlook.fr>
2020-10-23 02:36:22 +0200

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,21 @@ cities = %w[Marseille Paris Bordeaux Nice Monaco Strasbourg Amiens Rennes]
 cities.each do |c|
   City.create(city_name:c )
 end
+puts
+puts "Cities table"
+tp City.all
 
 50.times do
   Dog.create(name: Faker::Name.first_name, city: City.all.sample)
   Dogsitter.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city: City.all.sample)
 end
+puts
+puts "Dogs table"
+tp Dog.all
+
+puts
+puts "Dogsitters table"
+tp Dogsitter.all
 
 200.times do 
   random_city = City.all.reject {|c| c.dogs.empty? || c.dogsitters.empty?}.sample
@@ -21,3 +31,7 @@ end
   random_dogsitter_from_city = Dogsitter.where(city:random_city).sample
   Stroll.create(date: Faker::Time.forward(days: 90),dog: random_dog_from_city, dogsitter: random_dogsitter_from_city )
 end
+
+puts
+puts "Strolls table"
+tp Stroll.all
